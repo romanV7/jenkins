f9b17d008fad682b53fec1625f59642686d87b8a
Roman <mister02.rv@gmail.com>
2020-07-19 15:13:27 +0300

    jenkins

--- Jenkinsfile.js
+++ Jenkinsfile.js
@@ -19,10 +19,18 @@ pipeline {
         }
       }
     }
-    stage("Remove previos container") {
+    stage("Test") {
+      steps {
+        def dockerTest = "docker-compose -f docker-compose.yml -f docker-compose.test.yml up --force-recreate --abort-on-container-exit --exit-code-from test"
+        withCredentials([string(credentialsId: 'docker', variable: 'dockerPassword')]) {
+          sh "docker login -u ${DOCKER_DEV} -p ${dockerPassword}"
+          sh "${dockerTest}"
+        }
+      }
+    }
+    stage("Remove previos containers") {
       steps {
         try {
-          //def dockerRm = "docker rm -f ${DOCKER_APP}"
           def dockerRm = "docker-compose down"
       		sshagent(['docker-dev']) {
       			sh "ssh -o StrictHostKeyChecking=no ec2-user@172.31.17.196 ${dockerRm}"
@@ -32,7 +40,7 @@ pipeline {
     }
     stage("Deploy to dev server") {
       steps {
-        def dockerRun = 'docker-compose up -d'
+        def dockerRun = 'docker-compose -f docker-compose.yml up -d'
         sshagent(['docker-dev']) {
          sh "ssh -o StrictHostKeyChecking=no ec2-user@172.31.17.196 ${dockerRun}"
         }
