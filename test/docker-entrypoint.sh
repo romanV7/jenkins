3493dd2d7b485fbaab81a151a377212d534ee639
Roman <mister02.rv@gmail.com>
2020-07-19 15:04:22 +0300

    change

--- test/docker-entrypoint.sh
+++ test/docker-entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-
 echo -en "\033[37;1;42m Check start page: \033[0m"
-
-curl -H "Host: docker.host" --silent --show-error --fail -I http://172.17.0.1/
+curl -H "Host: docker.marsel.host" --silent --show-error --fail -I app:3000
